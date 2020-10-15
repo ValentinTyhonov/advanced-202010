23ab2ee4a263dea0b6cfebcf41e9cc17e4d30ec2
valentin-tyhonov <valentin.tyhonov@ancestry.com>
2020-10-15 22:39:49 +0300

    Pass attributes to JSP

--- lesson-3-servlets/src/main/java/org/logos/web/RegistrationServlet.java
+++ lesson-3-servlets/src/main/java/org/logos/web/RegistrationServlet.java
@@ -10,11 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 @Log4j
 @WebServlet("/register")
 public class RegistrationServlet extends HttpServlet
 {
+
+    private static List<User> users = new ArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
@@ -25,7 +30,7 @@ public class RegistrationServlet extends HttpServlet
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
         log.info("Inside doPost method.");
         User user = new User();
@@ -34,15 +39,20 @@ public class RegistrationServlet extends HttpServlet
         user.setEmail(req.getParameter("email"));
         user.setPassword(req.getParameter("password"));
 
+        users.add(user);
         log.info(user);
 
-        resp.setContentType("text/html");
-        try (PrintWriter printWriter = resp.getWriter())
-        {
-            printWriter.write("Welcome " + user.getFirstName() + " " + user.getLastName());
-        } catch (IOException e)
-        {
-            log.error(e.getMessage(), e);
-        }
+        req.setAttribute("users", users);
+
+        req.getRequestDispatcher("users.jsp").forward(req, resp);
+
+//        resp.setContentType("text/html");
+//        try (PrintWriter printWriter = resp.getWriter())
+//        {
+//            printWriter.write("Welcome " + user.getFirstName() + " " + user.getLastName());
+//        } catch (IOException e)
+//        {
+//            log.error(e.getMessage(), e);
+//        }
     }
 }
